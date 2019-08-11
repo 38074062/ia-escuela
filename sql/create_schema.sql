c2a47bc4e3ca3e624f287ae8b25376c2f91a918d
Ana Martinez Saucedo <anmartinez@uade.edu.ar>
2019-08-11 18:05:38 -0300

    Configuracion de la base de datos

---
+++ sql/create_schema.sql
@@ -0,0 +1,19 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+-- -----------------------------------------------------
+-- Schema escuela
+-- -----------------------------------------------------
+CREATE DATABASE escuela;
+USE escuela;
+
+CREATE USER usuario IDENTIFIED BY 'WbX6txS89B';
+
+GRANT ALL PRIVILEGES ON *.* TO usuario;
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
