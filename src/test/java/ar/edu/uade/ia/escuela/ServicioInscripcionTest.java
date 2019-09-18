182fec03f4902e6c5b5d8b09a6059e7ca65ea6da
RODRIGO FRAGUAS <rfraguas38@gmail.com>
2019-09-18 10:05:53 -0300

    Test de inscripcion y facturacion (andan)

---
+++ src/test/java/ar/edu/uade/ia/escuela/ServicioInscripcionTest.java
@@ -0,0 +1,87 @@
+package ar.edu.uade.ia.escuela;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import ar.edu.uade.ia.escuela.presentacion.dto.AlumnoDto;
+import ar.edu.uade.ia.escuela.presentacion.dto.EscolaridadDto;
+import ar.edu.uade.ia.escuela.presentacion.dto.InscripcionDto;
+import ar.edu.uade.ia.escuela.presentacion.dto.ServicioDto;
+import ar.edu.uade.ia.escuela.presentacion.dto.TitularDto;
+import ar.edu.uade.ia.escuela.servicio.ServicioInscripcion;
+import ar.edu.uade.ia.escuela.servicio.ServicioServicio;
+import ar.edu.uade.ia.escuela.servicio.ServicioTitular;
+
+@RunWith( SpringRunner.class )
+@SpringBootTest
+@DirtiesContext( classMode = ClassMode.AFTER_EACH_TEST_METHOD )
+public class ServicioInscripcionTest 
+{
+	@Autowired
+	private ServicioInscripcion inscripcion;
+	
+	@Autowired
+	private ServicioServicio servicio;
+	
+	@Autowired
+	private ServicioTitular titular;
+	
+	@Test
+	public void inscribirAlumno()
+	{
+		//Dado
+		EscolaridadDto escolaridadDto = new EscolaridadDto();
+		escolaridadDto.setId(01L);
+        escolaridadDto.setNombre( "Media jornada" );
+        escolaridadDto.setPrecio( 5000L );
+        servicio.altaEscolaridad( escolaridadDto );
+        
+        TitularDto titularDto = new TitularDto();
+        titularDto.setId(01L);
+        titularDto.setNombre("Juan");
+        titularDto.setApellido("Perez");
+        titularDto.setDireccion("Lima 775");
+        titularDto.setDni(12345678);
+        titularDto.setEmail("jperez@uade.edu.ar");
+        titularDto.setCuentaBancaria("3120941931203");
+        titular.altaTitular( titularDto );
+        
+        List<ServicioDto> servicios = servicio.listarServicios();
+        List<TitularDto> titulares = titular.listarTitulares();
+        
+        Long idT = titulares.get(0).getId();
+        ArrayList<Long> idS = new ArrayList<Long>();
+        for(ServicioDto s:servicios)
+        {
+        	idS.add(s.getId());
+        }
+               
+        
+        AlumnoDto alumno = new AlumnoDto();
+        alumno.setNombre("Pepe");
+        alumno.setApellido("Gomez");
+        alumno.setDni(186512345);
+        
+        InscripcionDto inscripcionDto = new InscripcionDto();
+        inscripcionDto.setAlumno(alumno);
+        inscripcionDto.setIdTitular(idT);
+        inscripcionDto.setIdServicios(idS);
+        
+        //Cuando
+		inscripcion.inscribirAlumno(inscripcionDto);
+		
+		//Entonces
+		assertEquals(1, inscripcion.listarInscripciones().size());
+	}
+
+}
